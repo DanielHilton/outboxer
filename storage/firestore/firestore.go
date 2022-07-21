402f26469d4f7b6e940e18a3b61694ee9ac8f3d2
Daniel Hilton <daniellawrencehilton@hotmail.co.uk>
2022-07-21 10:57:01 +0100

    :construction: `GetEvents`

--- storage/firestore/firestore.go
+++ storage/firestore/firestore.go
@@ -2,11 +2,13 @@ package firestore
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"time"
 
 	"cloud.google.com/go/firestore"
 	"github.com/italolelis/outboxer"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
 
@@ -22,7 +24,16 @@ type Firestore struct {
 	CollectionName string
 }
 
-// WithInstance creates a firestore data store with the required information to make a connection..
+type firestoreOutboxMessage struct {
+	ID           int64                  `firestore:"id"`
+	Dispatched   bool                   `firestore:"dispatched"`
+	DispatchedAt time.Time              `firestore:"dispatched_at"`
+	Payload      string                 `firestore:"payload"`
+	Options      outboxer.DynamicValues `firestore:"options"`
+	Headers      outboxer.DynamicValues `firestore:"headers"`
+}
+
+// WithInstance creates a firestore data store with the required information to make a connection.
 func WithInstance(ctx context.Context, projectID string, collectionName string, firestoreClientOptions ...option.ClientOption) (*Firestore, error) {
 	c, err := firestore.NewClient(ctx, projectID, firestoreClientOptions...)
 	if err != nil {
@@ -49,8 +60,39 @@ func (f *Firestore) Close() error {
 }
 
 func (f *Firestore) GetEvents(ctx context.Context, batchSize int32) ([]*outboxer.OutboxMessage, error) {
-	//TODO implement me
-	panic("implement me")
+	var events []*outboxer.OutboxMessage
+
+	docs := f.client.Collection(f.CollectionName).Where("dispatched", "==", false).Documents(ctx)
+	for {
+		doc, err := docs.Next()
+		if err != nil {
+			if err == iterator.Done {
+				return events, nil
+			}
+
+			return nil, fmt.Errorf("failed to get messages from store: %w", err)
+		}
+
+		var f firestoreOutboxMessage
+		err = doc.DataTo(&f)
+		if err != nil {
+			return nil, fmt.Errorf("malformed event found, error: %w", err)
+		}
+
+		e := outboxer.OutboxMessage{
+			ID:         f.ID,
+			Dispatched: f.Dispatched,
+			DispatchedAt: sql.NullTime{
+				Time:  f.DispatchedAt,
+				Valid: true,
+			},
+			Payload: []byte(f.Payload),
+			Options: f.Options,
+			Headers: f.Headers,
+		}
+
+		events = append(events, &e)
+	}
 }
 
 func (f *Firestore) Add(ctx context.Context, m *outboxer.OutboxMessage) error {
